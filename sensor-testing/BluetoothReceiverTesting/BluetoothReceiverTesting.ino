c7d67464d577a3262a00fe70c326c64a16ce48f9
TjerkR <TjerkR@users.noreply.github.com>
2016-11-05 17:06:28 +0100

    Added program for testing the Bluetooth receiver

---
+++ sensor-testing/BluetoothReceiverTesting/BluetoothReceiverTesting.ino
@@ -0,0 +1,15 @@
+#include <SoftwareSerial.h>
+
+#define receiverPin 0
+#define transmitterPin 1
+
+void setup() {
+  SoftwareSerial.begin();
+  SoftwareSerial(receiverPin,transmitterPin);
+}
+
+void loop() {
+  char c = Serial.read();
+  if ( c ==
+
+}
