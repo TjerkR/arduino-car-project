4a55daf6b990abcde897b57741ee9461b505a31d
TjerkR <tjerk.reintsema@gmail.com>
2016-11-09 10:29:51 +0100

    Rewrote ReflectionSensorTesting

--- sensor-testing/ReflectionSensorTesting/ReflectionSensorTesting.ino
+++ sensor-testing/ReflectionSensorTesting/ReflectionSensorTesting.ino
@@ -1,18 +1,32 @@
 /** Arduino Car System: Color Sensor Testing
  */
 
-#define LightSensor A0
+#define leftLightSensor A0
+#define rightLightSensor A1
 
 /* These functions return the reflection measured by either of the bottom light sensors.
  * The output is a value between 0 and 100, where 0 is maximum reflection and 100 is no reflection. */
-int checkLine() {return map(analogRead(LightSensor), 0, 1023, 0, 100);}
+int checkLineLeft() 
+{
+  return map(analogRead(leftLightSensor), 0, 1023, 0, 100);
+}
+
+int checkLineRight() 
+{
+  return map(analogRead(rightLightSensor), 0, 1023, 0, 100);
+}
 
 void setup() {
   Serial.begin(9600);
   digitalWrite(A0,HIGH);
+  digitalWrite(A1,HIGH);
 }
 
 void loop() {
-  Serial.print("Sensor data: \t");
-  Serial.println(analogRead(LightSensor));
+  Serial.print("Left: \t");
+  Serial.print(analogRead(leftLightSensor));
+  Serial.print("\t");
+  Serial.print("Right: \t");
+  Serial.println(analogRead(rightLightSensor));
+  
 }
