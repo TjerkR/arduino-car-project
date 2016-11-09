7478b491b40841765ff004aef501b455597e584d
TjerkR <tjerk.reintsema@gmail.com>
2016-11-09 14:48:22 +0100

    Updated IR sensor test

--- sensor-testing/InfraRedSensorTesting/InfraRedSensorTesting.ino
+++ sensor-testing/InfraRedSensorTesting/InfraRedSensorTesting.ino
@@ -32,15 +32,17 @@ float sampleVoltage(int pin) {
 
 /* These functions return the distance measured by either of the IR sensors on the front of the car. 
  * This is a value between 3 and 40 cm - it doesn't work properly outside of this range */
-int checkDistanceLeft() {return (2600.0 / sampleVoltage(leftIRsensor) - 0.148);}
-// int checkDistanceRight() {return (2600.0 / sampleVoltage(rightIRsensor) - 0.148);}
+//int checkDistanceLeft() {return (12000.0 / sampleVoltage(leftIRsensor) - 3.5);}
+//int checkDistanceRight() {return (12000.0 / sampleVoltage(rightIRsensor) - 3.5);}
+int checkDistanceLeft() {return sampleVoltage(leftIRsensor);}
+int checkDistanceRight() {return sampleVoltage(rightIRsensor);}
 
 
 /* This function feeds sensor data to the serial port. This report is visible by going to Tools->Serial Monitor, while the
  * Arduino is hooked up and running. */
 void report() {
   Serial.print("DistanceLeft: \t"); Serial.print(checkDistanceLeft()); Serial.println("\t");
-  // Serial.print("DistanceRight: \t");Serial.print(checkDistanceRight()); Serial.print("\t");
+  Serial.print("DistanceRight: \t");Serial.print(checkDistanceRight()); Serial.print("\t");
 }
 
 
